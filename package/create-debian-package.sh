8d6ed5f5e8e7e0d9bfd8e07d653f6220fc85fc45
Rafał Malinowski <rafal.malinowski@corp.bluecherry.net>
2013-05-03 15:17:59 +0200

    Install cmake when creating package
    Signed-off-by: Rafał Malinowski <rafal.malinowski@corp.bluecherry.net>

--- package/create-debian-package.sh
+++ package/create-debian-package.sh
@@ -15,7 +15,7 @@ if [ "!" == "!$VERSION" ]; then
         exit
 fi
 
-sudo apt-get install build-essential devscripts debhelper
+sudo apt-get install build-essential devscripts debhelper cmake
 sudo apt-get install git-core
 sudo apt-get install libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
 
