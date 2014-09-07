4d3544cc94b65f9faad2f65821be0eafce3826d3
Anton <anton@corp.bluecherry.net>
2014-09-07 17:07:42 +0400

    Updated download URL for Qt sources
    Removed -no-dbus configure option

--- dependencies/install-qt-linux.sh
+++ dependencies/install-qt-linux.sh
@@ -3,7 +3,7 @@
 QT_SRC_VERSION=4.8.5
 QT_SRC_FILE=qt-everywhere-opensource-src-$QT_SRC_VERSION.tar.gz
 QT_SRC_DIR=qt-everywhere-opensource-src-$QT_SRC_VERSION
-QT_SRC_URL=http://download.qt-project.org/official_releases/qt/4.8/${QT_SRC_VERSION}/$QT_SRC_FILE
+QT_SRC_URL=http://download.qt-project.org/archive/qt/4.8/${QT_SRC_VERSION}/$QT_SRC_FILE
 
 function installQt {
     installBuildDependencies
@@ -43,7 +43,7 @@ function patchQtSources {
 function buildQtFromSources {
     ./configure -prefix /usr/lib/bluecherry/qt4.8/ -confirm-license -opensource \
       -no-qt3support -no-xmlpatterns -openssl -opengl desktop -webkit -gtkstyle \
-      -qtlibinfix -bluecherry -nomake demos -nomake examples -no-dbus \
+      -qtlibinfix -bluecherry -nomake demos -nomake examples  \
       -no-multimedia -no-audio-backend -no-phonon -no-phonon-backend -no-svg \
       -script -no-scripttools -declarative -no-declarative-debug -rpath -release
     make -j5
