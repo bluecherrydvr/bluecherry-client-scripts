aabfe32c502dfa9370921d91521c50e510d27c6b
Anton <anton@corp.bluecherry.net>
2014-09-22 01:26:06 +0400

    Fixed ffmpeg configure parameters

--- dependencies/install-ffmpeg-linux.sh
+++ dependencies/install-ffmpeg-linux.sh
@@ -29,8 +29,8 @@ function unpackFFmpegSources {
 }
 
 function buildFFmpegFromSources {
-    ./configure --prefix=/usr/lib/bluecherry/client/ --enable-shared --disable-programs --enable-pic --disable-stripping --disable-doc --disable-everything --enable-protocol=file --enable-protocol=pipe --enable-protocol=http --enable-muxer=matroska --enable-muxer=mjpeg --enable-muxer=rtp --enable-demuxer=rtsp --enable-demuxer=matroska --enable-demuxer=mjpeg --enable-decoder=h264 --enable-decoder=mpeg4 --enable-decoder=mjpeg --enable-parser=h264 --enable-parser=mpeg4video --enable-parser=mjpeg --enable-encoder=mjpeg
-    make -j5
+    ./configure --prefix=/usr/lib/bluecherry/client/ --enable-shared --disable-programs --enable-pic --disable-stripping --disable-doc  --enable-protocol=file --enable-protocol=pipe --enable-protocol=http --enable-muxer=matroska --enable-muxer=mjpeg --enable-muxer=rtp --enable-demuxer=rtsp --enable-demuxer=matroska --enable-demuxer=mjpeg --enable-decoder=h264 --enable-decoder=mpeg4 --enable-decoder=mjpeg --enable-parser=h264 --enable-parser=mpeg4video --enable-parser=mjpeg --enable-encoder=mjpeg --disable-lzma
+    make -j8
 }
 
 function installFFmpegFromSources {
