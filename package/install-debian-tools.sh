4237149e22fe48ad604cc183b958eaff21eac294
Anton <anton@staff.bluecherrydvr.com>
2017-02-15 21:08:45 +0400

    added .deb build time dependencies

--- package/install-debian-tools.sh
+++ package/install-debian-tools.sh
@@ -2,4 +2,5 @@
 
 sudo apt-get install build-essential devscripts debhelper cmake
 sudo apt-get install git-core
-sudo apt-get install libasound2-dev libva-dev libqtwebkit-dev libqt4-dev
+sudo apt-get install libasound2-dev libva-dev libqtwebkit-dev libqt4-dev libpulse-dev yasm zlib1g-dev
+
