20bc2c0e6a43005c43488353d153ce8fa75ba3bb
AlexA <myis@email.com>
2017-04-07 17:45:20 +0400

    Added libmpv install scripts for linux and mac

---
+++ dependencies/install-libmpv-macosx.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+LIBMPV_SRC_DIR=mpv
+LIBMPV_SRC_URL=https://github.com/mpv-player/$LIBMPV_SRC_DIR.git
+
+function installLibMPV {
+    installBuildDependencies
+    downloadLibMPVSources
+
+    pushd $LIBMPV_SRC_DIR
+    buildLibMPVFromSources
+    installLibMPVFromSources
+    popd
+}
+
+function installBuildDependencies {
+    echo "no dependencies"
+}
+
+function downloadLibMPVSources {
+    git clone $LIBMPV_SRC_URL
+}
+
+function buildLibMPVFromSources {
+
+    ./bootstrap.py
+
+    PKG_CONFIG_PATH=/usr/lib/pkgconfig:/usr/local/lib/pkgconfig:$HOME/bc-dev/ffmpeg/lib/pkgconfig \
+    CFLAGS="-I$HOME/bc-dev/ffmpeg/include" \
+    LDFLAGS="-L$HOME/bc-dev/ffmpeg/lib -Wl,-rpath,$HOME/bc-dev/ffmpeg/lib" \
+    ./waf configure --prefix=$HOME/bc-dev/libmpv --disable-cplayer --enable-libmpv-shared --disable-libass --disable-manpage-build
+
+    ./waf build
+}
+
+function installLibMPVFromSources {
+    ./waf install
+}
+
+installLibMPV
+
