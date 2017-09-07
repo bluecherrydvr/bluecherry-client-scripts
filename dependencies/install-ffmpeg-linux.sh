f370c59b7d2855c6ea747175b2424d35d4c92baf
Anton <anton@corp.bluecherry.net>
2017-09-07 13:13:46 +0400

    enabled HEVC/H.265 and updated version

--- dependencies/install-ffmpeg-linux.sh
+++ dependencies/install-ffmpeg-linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FFMPEG_SRC_VERSION=3.2.4
+FFMPEG_SRC_VERSION=3.3.3
 FFMPEG_SRC_FILE=ffmpeg-$FFMPEG_SRC_VERSION.tar.bz2
 FFMPEG_SRC_DIR=ffmpeg-$FFMPEG_SRC_VERSION
 FFMPEG_SRC_URL=http://ffmpeg.org/releases/$FFMPEG_SRC_FILE
@@ -29,7 +29,7 @@ function unpackFFmpegSources {
 }
 
 function buildFFmpegFromSources {
-    ./configure --prefix=/usr/lib/bluecherry/client/ --enable-shared --disable-programs --enable-pic --disable-stripping --disable-doc  --enable-protocol=file --enable-protocol=pipe --enable-protocol=http --enable-muxer=matroska --enable-muxer=mjpeg --enable-muxer=rtp --enable-demuxer=rtsp --enable-demuxer=matroska --enable-demuxer=mjpeg --enable-decoder=h264 --enable-decoder=mpeg4 --enable-decoder=mjpeg --enable-parser=h264 --enable-parser=mpeg4video --enable-parser=mjpeg --enable-encoder=mjpeg --disable-lzma
+    ./configure --prefix=/usr/lib/bluecherry/client/ --enable-shared --disable-programs --enable-pic --disable-stripping --disable-doc  --enable-protocol=file --enable-protocol=pipe --enable-protocol=http --enable-muxer=matroska --enable-muxer=mjpeg --enable-muxer=rtp --enable-demuxer=rtsp --enable-demuxer=matroska --enable-demuxer=mjpeg --enable-decoder=h264 --enable-decoder=hevc --enable-decoder=mpeg4 --enable-decoder=mjpeg --enable-parser=h264 --enable-parser=hevc --enable-parser=mpeg4video --enable-parser=mjpeg --enable-encoder=mjpeg --disable-lzma --enable-hwaccel=hevc_vaapi --enable-hwaccel=h264_vaapi --enable-vaapi
     make -j8
 }
 
